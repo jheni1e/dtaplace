c76a035dadfc645e12204fe78c95b1845999bdb8
Leehzitha <leticia.burlinski@gmail.com>
2025-08-08 16:47:47 -0300

    Entrega 5 - Parcial (Payloads)

--- obj/Debug/net9.0/dtaplace.AssemblyInfo.cs
+++ obj/Debug/net9.0/dtaplace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dtaplace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21e3c4e5c98373f25d9d2190e6eb40f1b15d8e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b7b4f31d97cd6061079e3e06cd2112cde941c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dtaplace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dtaplace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
