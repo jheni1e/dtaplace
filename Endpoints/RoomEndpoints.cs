1d8f6c90e55526a16afa7f6a2a8b259a2adf9b69
Jhenie <highhalma@gmail.com>
2025-08-08 15:53:12 -0300

    Entrega 6 - completa: endpoints configurados

--- Endpoints/RoomEndpoints.cs
+++ Endpoints/RoomEndpoints.cs
@@ -1,11 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace dtaplace.Endpoints;
 
 public static class RoomEndpoints
 {
     public static void ConfigureRoomEndpoints(this WebApplication app)
     {
-        app.MapGet("rooms", ([FromServices]GetRoomsUseCase) =>
+        app.MapGet("rooms", ([FromServices] GetRoomsUseCase useCase) =>
         {
+            var result = await useCase.Do();
+            if (result.IsSuccess)
+                return Results.Ok();
+
+            return Results.BadRequest(result.Reason);
+        });
+
+        app.MapPost("create/room", (
+            [FromBody] CreateRoomPayload payload,
+            [FromServices] CreateRoomUseCase useCase) =>
+        {
+            var result = await useCase.Do(payload);
+            if (result.IsSuccess)
+                return Results.Ok();
+
+            return Results.BadRequest(result.Reason);
+        });
+
+        app.MapGet("roles", ([FromServices] GetRolesUseCase useCase) =>
+        {
+            var result = await useCase.Do();
+            if (result.IsSuccess)
+                return Results.Ok();
+
+            return Results.BadRequest(result.Reason);
+        });
+
+        app.MapPost("create/role", (
+            [FromBody] CreateRolePayload payload,
+            [FromServices] CreateRoleUseCase useCase) =>
+        {
+            var result = await useCase.Do(payload);
+            if (result.IsSuccess)
+                return Results.Ok();
+
+            return Results.BadRequest(result.Reason);
+        });
+        
+        app.MapPost("invite/{username}", (
+            string username,
+            [FromServices] SendInvitationUseCase useCase) =>
+        {
+            var payload = new SendInvitationPayload(username);
             var result = await useCase.Do(payload);
             if (result.IsSuccess)
                 return Results.Ok();
