df5a12bc43ceaf50dd8abf313b223a413ffe2e33
Jhenie <highhalma@gmail.com>
2025-09-05 16:53:28 -0300

    dbcontext atualizado

--- Models/DTAPlaceDbContext.cs
+++ Models/DTAPlaceDbContext.cs
@@ -17,28 +17,45 @@ public class DTAPlaceDbContext(DbContextOptions<DTAPlaceDbContext> opts) : DbCon
     protected override void OnModelCreating(ModelBuilder mb)
     {
         mb.Entity<UserRoom>()
-            .HasOne(ur => ur.User)
-            .WithMany(u => u.UserRooms)
-            .OnDelete(DeleteBehavior.NoAction);
+        .HasOne(ur => ur.User)
+        .WithMany(u => u.UserRooms)
+        .HasForeignKey(ur => ur.UserID)
+        .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<UserRoom>()
             .HasOne(ur => ur.Room)
             .WithMany(r => r.UserRooms)
+            .HasForeignKey(ur => ur.RoomID)
             .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<UserRoom>()
             .HasOne(ur => ur.Role)
             .WithOne(r => r.UserRoom)
+            .HasForeignKey<UserRoom>(ur => ur.RoleID)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        mb.Entity<User>()
+            .HasOne(u => u.Plan)
+            .WithMany(p => p.Users)
+            .HasForeignKey(u => u.PlanID)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        mb.Entity<Room>()
+            .HasOne(r => r.Creator)
+            .WithMany(u => u.Rooms)
+            .HasForeignKey(r => r.CreatorID)
             .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<Invitation>()
             .HasOne(r => r.Receiver)
             .WithMany(i => i.Invitations)
+            .HasForeignKey(i => i.ReceiverID)
             .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<Invitation>()
             .HasOne(i => i.Room)
             .WithMany(r => r.Invitations)
+            .HasForeignKey(r => r.RoomID)
             .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<Plan>()
@@ -49,11 +66,13 @@ public class DTAPlaceDbContext(DbContextOptions<DTAPlaceDbContext> opts) : DbCon
         mb.Entity<Pixel>()
             .HasOne(p => p.Room)
             .WithMany(r => r.Pixels)
+            .HasForeignKey(r => r.RoomID)
             .OnDelete(DeleteBehavior.NoAction);
 
         mb.Entity<GiftCard>()
             .HasOne(g => g.Plan)
             .WithMany(p => p.GiftCards)
+            .HasForeignKey(p => p.PlanID)
             .OnDelete(DeleteBehavior.NoAction);
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -8,6 +8,7 @@ public class Room
     public int Height { get; set; }
 
     public required User Creator { get; set; }
+    public int CreatorID { get; set; }
 
     public ICollection<UserRoom> UserRooms { get; set; } = [];
     public ICollection<Pixel> Pixels { get; set; } = [];
